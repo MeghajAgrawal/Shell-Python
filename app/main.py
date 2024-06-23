24fb7e7be8e3eeaed7001265ea3b4cebfd98d351
Meghaj Agrawal <meghajcr7@gmail.com>
2024-06-23 16:02:20 -0400

    POSIX 4th Stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    valid_commands = []
+    valid_commands = ['exit 0']
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -12,7 +12,9 @@ def main():
         if cmd not in valid_commands:
             sys.stdout.write(f"{cmd}: command not found\n")
             continue
-        
+        else:
+            if cmd.lower() == 'exit 0':
+                break
 
 
 if __name__ == "__main__":
