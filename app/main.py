91d133c0d1b03f35c5d00f14c8fbad51eb7b9300
Meghaj Agrawal <meghajcr7@gmail.com>
2024-06-23 16:06:06 -0400

    POSIX 5th Stage

--- app/main.py
+++ app/main.py
@@ -3,19 +3,19 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    valid_commands = ['exit 0']
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        cmd = input()
-        if cmd not in valid_commands:
-            sys.stdout.write(f"{cmd}: command not found\n")
-            continue
-        else:
+        if cmd := input():
             if cmd.lower() == 'exit 0':
                 break
-
+            elif 'echo' in cmd.lower():
+                msg = cmd[len("echo "):]
+                sys.stdout.write(f"{msg}" + "\n")
+            else:
+                sys.stdout.write(f"{cmd}: command not found\n")
+        continue
 
 if __name__ == "__main__":
     main()
